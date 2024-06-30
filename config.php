48da95c28f7cfa70d84c24a1884558779a256723
HudsonJardim <113267492+HudsonJardim@users.noreply.github.com>
2024-06-30 11:28:52 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$_server = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'formulario';
+
+$conn = new mysqli ('localhost', 'root', '', 'formulario');
+
+
+if($conn -> connect_error) {
+die ("falha ao se comunicar com banco de dados:" .$conn->connect_error);
+}
+?>
